a8a3f3d2c0df453fed086510432663558fa3f438
Albert Hafvenström <albhaf@gmail.com>
2021-11-16 10:41:50 +0100

    fish: update pyenv init

--- config.fish
+++ config.fish
@@ -21,6 +21,9 @@ if type -q rbenv
 end
 
 if type -q pyenv
+  set -Ux PYENV_ROOT $HOME/.pyenv
+  set -Ux fish_user_paths $PYENV_ROOT/bin $fish_user_paths
+  status is-login; and pyenv init --path | source
   pyenv init - | source
 end
 
